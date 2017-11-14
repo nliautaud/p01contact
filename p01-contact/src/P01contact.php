cf0c7e2d47801e4465ed6cdd4bdce52f053a8610
nliautaud <nicolas.liautaud@gmail.com>
2017-11-14 10:17:58 +0100

    upd version

--- p01-contact/src/P01contact.php
+++ p01-contact/src/P01contact.php
@@ -21,7 +21,7 @@ class P01contact
 
     public function __construct()
     {
-        define('P01C\VERSION', '1.1.0');
+        define('P01C\VERSION', '1.1.2');
         $this->version = VERSION;
 
         define('P01C\SERVERNAME', $_SERVER['SERVER_NAME']);
